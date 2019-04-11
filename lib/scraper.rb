794774b39dfa5d6447d6110baaace26fb15023de
Arthur Song <arth3rs0ng@gmail.com>
2019-04-11 16:15:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,9 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  #we don't to create instances of Scraper that maintain their own attributes
+  #we just need to scrap info and pass to student
+  #therefore we only need class methods
 
   def self.scrape_index_page(index_url) #return an array of hashes, each has represent a student
     #have keys name, location, and profile url
