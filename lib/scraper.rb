a93606e4a1320ef049cf1d6dc7c3586c72bbe300
Arthur Song <arth3rs0ng@gmail.com>
2019-04-11 16:14:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,8 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
+  def self.scrape_index_page(index_url) #return an array of hashes, each has represent a student
+    #have keys name, location, and profile url
     
   end
 
